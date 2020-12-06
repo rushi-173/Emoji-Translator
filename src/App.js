b494596472586a0b8870e49484b5c0d75d8d490a
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2020-12-06 20:52:08 +0530

    change

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ export default function App() {
                 cursor: "pointer",
                 padding: "0.5rem"
               }}
-              onClick={() => clickEmojiHandler()}
+              onClick={() => clickEmojiHandler(item)}
             >
               {item}
             </span>
