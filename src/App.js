e5f7687b69e86eb5c6f41533469239777822a297
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2020-12-06 14:47:45 +0530

    ex-07

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  let likeCounter = 0;
   function likeBtnHandler() {
-    console.log("liked");
+    likeCounter += 1;
+    console.log(likeCounter);
   }
   return (
     <div className="App">
