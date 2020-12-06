a748e453932fdc21d2d47a1277459b5a819a2080
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2020-12-06 16:20:20 +0530

    upto ex-11

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+var emojiDictionary = {
+  "😊": "Happy",
+  "😔": "Sad",
+  "❤️": "Love",
+  "👋": "Hello",
+  "🧠": "Brain",
+  "🔥": "Fire",
+  "⚽": "FootBall",
+  "⚾": "Baseball",
+  "🏀": "Basketball",
+  "🏐": "Volleyball"
+};
+
 export default function App() {
   var [emoji, setEmoji] = useState();
   function inputHandler(event) {
     setEmoji(event.target.value);
-    console.log(event.target.value);
+    if (emoji in emojiDictionary) {
+      console.log(emojiDictionary[emoji]);
+    } else {
+      console.log("Dont know about this.");
+    }
   }
   return (
     <div className="App">
