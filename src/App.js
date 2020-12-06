ae63b5d3d39bbdb12e5daa4727d2371b63adbc19
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2020-12-06 15:09:17 +0530

    upto ex-08

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let likeCounter = 0;
+  var [likeCount, setLikeCounter] = useState(0);
   function likeBtnHandler() {
-    likeCounter += 1;
-    console.log(likeCounter);
+    setLikeCounter((likeCount += 1));
+    console.log(likeCount);
   }
   return (
     <div className="App">
       <h1 className="mainHeading">Emoji Translator</h1>
       <button onClick={likeBtnHandler}> Like </button>
+      <h2> {likeCount} </h2>
     </div>
   );
 }
