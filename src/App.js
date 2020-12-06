8489c5fd4af89728af34a0f98004d43da8066d1f
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2020-12-06 20:07:33 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -40,33 +40,35 @@ export default function App() {
       <nav>
         <h1 className="mainHeading">Emoji Translator</h1>
       </nav>
-      <input
-        type="text"
-        onChange={() => inputHandler()}
-        style={{ width: "70%", padding: "0.4rem" }}
-        placeholder="Paste your emoji here!"
-      />
-      <p style={{ marginTop: "2rem", marginBottom: "0", fontSize: "2rem" }}>
-        {emoji}
-      </p>
-      <p style={{ marginTop: "0rem", fontSize: "2rem", fontWeight: "bold" }}>
-        {meaning}
-      </p>
-      {emojiList.map((item) => {
-        return (
-          <span
-            key={item}
-            style={{
-              fontSize: "x-large",
-              cursor: "pointer",
-              padding: "0.5rem"
-            }}
-            onClick={() => clickEmojiHandler(item)}
-          >
-            {item}
-          </span>
-        );
-      })}
+      <section>
+        <input
+          type="text"
+          onChange={() => inputHandler()}
+          style={{ width: "70%", padding: "0.4rem" }}
+          placeholder="Paste your emoji here!"
+        />
+        <p style={{ marginTop: "2rem", marginBottom: "0", fontSize: "2rem" }}>
+          {emoji}
+        </p>
+        <p style={{ marginTop: "0rem", fontSize: "2rem", fontWeight: "bold" }}>
+          {meaning}
+        </p>
+        {emojiList.map((item) => {
+          return (
+            <span
+              key={item}
+              style={{
+                fontSize: "x-large",
+                cursor: "pointer",
+                padding: "0.5rem"
+              }}
+              onClick={() => clickEmojiHandler(item)}
+            >
+              {item}
+            </span>
+          );
+        })}
+      </section>
       <footer className="footer">
         <p className="footer-title"> Made with love by rushi. </p>
       </footer>
