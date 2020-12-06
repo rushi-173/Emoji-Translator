56a65eab1ec6fa85aeec78f41b8f368ff58baa6e
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2020-12-06 14:38:01 +0530

    upto ex-04

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  var name = "Rushikesh";
   return (
     <div className="App">
-      <h1>Emoji Translator</h1>
+      <h1 className="mainHeading">Emoji Translator</h1>
+      <h2>
+        Welcome <span Style="Color: red;">{name}</span>
+      </h2>
     </div>
   );
 }
